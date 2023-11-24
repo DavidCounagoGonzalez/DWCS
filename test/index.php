0596e70e4e3f802d657ac77579ac8b6f1f742530
David Couñago González <david.counagogonzalez@daw2-12.dip.local>
2023-11-24 14:30:32 +0100

    Dejo de ir

--- test/index.php
+++ test/index.php
@@ -12,29 +12,35 @@
     <body>
         <h1>Sombrero Seleccionador</h1>
         <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST">
-            <p>Cuál es tu color favorito?</p>
+            <p>Pregunta 1</p>
             <input type="radio" id="p1r1" name="preg1" value="1">
-            <label for="p1r1">Rosa</label> 
+            <label for="p1r1">R1</label> 
             <input type="radio" id="p1r2" name="preg1" value="2">
-            <label for="p1r2">Negro</label>
+            <label for="p1r2">R2</label>
             <input type="radio" id="p1r3" name="preg1" value="3">
-            <label for="p1r3">Merequetengue</label> 
+            <label for="p1r3">R3</label> 
+            <input type="radio" id="p1r4" name="preg1" value="4">
+            <label for="p1r4">R4</label> 
             
-            <p>Cuál es tu comida favorita</p>
+            <p>Pregunta 2</p>
             <input type="radio" id="p2r1" name="preg2" value="3">
-            <label for="p2r1">Merequetengue</label> 
-            <input type="radio" id="p2r2" name="preg2" value="1">
-            <label for="p2r2">Como de todo</label>
-            <input type="radio" id="p2r3" name="preg2" value="2">
-            <label for="p2r3">La cocina asática</label> 
+            <label for="p2r1">R1</label> 
+            <input type="radio" id="p2r2" name="preg2" value="2">
+            <label for="p2r2">R2</label>
+            <input type="radio" id="p2r3" name="preg2" value="4">
+            <label for="p2r3">R3</label> 
+            <input type="radio" id="p2r4" name="preg2" value="1">
+            <label for="p2r4">R4</label> 
             
-            <p>Que haces en tus tiempos libres</p>
+            <p>Pregunta 3</p>
             <input type="radio" id="p3r1" name="preg3" value="2">
-            <label for="p3r1">Entrenar para ninja</label> 
+            <label for="p3r1">R1</label> 
             <input type="radio" id="p3r2" name="preg3" value="3">
-            <label for="p3r2">Merequetengue</label>
+            <label for="p3r2">R2</label>
             <input type="radio" id="p3r3" name="preg3" value="1">
-            <label for="p3r3">Comer</label>  
+            <label for="p3r3">R3</label> 
+            <input type="radio" id="p3r4" name="preg3" value="4">
+            <label for="p3r4">R4</label> 
             
             <br>
             <input type="submit" name="enviar" value="Enviar">
@@ -46,7 +52,7 @@
                 $archivo = fopen("Registro.txt", "w+") or die("No se puede abrir");
                 
                 if(empty($_SESSION['nombre'])){
-                    $contenido = fread($archivo);
+                    $contenido = fread($archivo, filesize("Registro.txt"));
                     $_SESSION['nombre'] = $contenido;
                 }else{
                     fwrite($archivo, "");
@@ -59,27 +65,21 @@
                     $suma = $_POST['preg1'] + $_POST['preg2'] + $_POST['preg3'];
                    
                     switch ($suma){
-                        case $suma<4: 
-                            $cadena = $_SESSION['nombre'] . " , eres Kirby";
+                        case $suma<6: 
+                            $cadena = $_SESSION['nombre'] . " ,eres el esto";
                             echo $cadena;
-                            echo "<br>";
-                            echo "<img src='imgs/kirbyvlc.jpeg'>";
                             fwrite($archivo, $cadena);
                             break;
                         
-                        case $suma>6:
-                            $cadena = $_SESSION['nombre'] . " ,  eres EL Merequetengue";
+                        case $suma>9:
+                            $cadena = $_SESSION['nombre'] . " ,eres el aquello";
                             echo $cadena;
-                            echo "<br>";
-                            echo "<img src='imgs/merequetengue.jpeg'>";
                             fwrite($archivo, $cadena);
                             break;
                         
                         default:
-                            $cadena = $_SESSION['nombre'] . " , eres Sergio";
+                            $cadena = $_SESSION['nombre'] . " ,eres el eso";
                             echo $cadena;
-                            echo "<br>";
-                            echo "<img src='imgs/sergiofoty.jpeg'>";
                             fwrite($archivo, $cadena);
                             break;
                    }
